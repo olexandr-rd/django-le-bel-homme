51239a6ed566a82aa25387e125b905e2de76ca45
olexandr_rd <sasha2002rudenko@gmail.com>
2023-04-27 13:02:34 +0300

    little bug fix

--- le_bel_homme/views.py
+++ le_bel_homme/views.py
@@ -32,13 +32,10 @@ def login_view(request):
 
 def register(request):
     if request.method == 'POST':
-        if request.method == 'POST':
-            form = UserRegistrationForm(request.POST)
-            if form.is_valid():
-                form.save()
-                return redirect('index ')
-        else:
-            form = UserRegistrationForm()
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
     else:
         form = UserRegistrationForm()
     return render(request, 'register.html', {'form': form})
